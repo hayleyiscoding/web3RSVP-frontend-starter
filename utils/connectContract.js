3ffead25c9259a7b33adad0b83be338ecf18eda6
Hayley <hayleyiscoding@gmail.com>
2022-08-07 12:31:20 +0530

    set up front-end

---
+++ utils/connectContract.js
@@ -0,0 +1,25 @@
+import abiJSON from "./Web3RSVP.json";
+import { ethers } from "ethers";
+
+function connectContract() {
+    const contractAddress = "0x00Dd672d0886825ee2413aAbdf3300F000e09585";
+    const contractABI = abiJSON.abi;
+    let rsvpContract;
+    try {
+      const { ethereum } = window;
+  
+      if (ethereum) {
+        //checking for eth object in the window
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        rsvpContract = new ethers.Contract(contractAddress, contractABI, signer); // instantiating new connection to the contract
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log("ERROR:", error);
+    }
+    return rsvpContract;
+  }
+  
+  export default connectContract;
